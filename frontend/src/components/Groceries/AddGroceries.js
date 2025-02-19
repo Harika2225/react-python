4c6e09881805a0decade2f135d721553e472623e
Harika2225 <harikasree2k@gmail.com>
2025-02-19 18:58:10 +0530

    add item on enter

--- frontend/src/components/Groceries/AddGroceries.js
+++ frontend/src/components/Groceries/AddGroceries.js
@@ -1,26 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { MdAddCircle } from "react-icons/md";
 
 const AddGroceriesForm = ({ addGroceries }) => {
-  const [groceriesName, setGroceriesName] = useState('');
+  const [groceriesName, setGroceriesName] = useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = () => {
     if (groceriesName) {
       addGroceries(groceriesName);
-      setGroceriesName('');
+      setGroceriesName("");
     }
+  };
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") handleSubmit();
   };
 
   return (
-    <div className='addGrocery'>
+    <div className="addGrocery">
       <input
         type="text"
         value={groceriesName}
         onChange={(e) => setGroceriesName(e.target.value)}
+        onKeyDown={handleKeyDown}
         placeholder="Enter grocery name"
       />
-      <button className='addGroceryIcon' type="submit" onClick={handleSubmit}><MdAddCircle/></button>
+      <button className="addGroceryIcon" type="submit" onClick={handleSubmit}>
+        <MdAddCircle />
+      </button>
     </div>
   );
 };
